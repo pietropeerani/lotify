7e96bcd8355a325f379938991c998229c7c1689d
pietro <pietro.peerani@gmail.com>
2025-06-17 11:54:23 +0200

    add: react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
 
 export default function App() {
   return (
-    <></>
+    <>
+      <Routes>
+        <Route />
+      </Routes>
+    </>
   )
 }
