649452050b5eaf2c4b06ae5f9c9f4ff2b2987c80
pietro <pietro.peerani@gmail.com>
2025-06-20 20:24:44 +0200

    add pages components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
 
 export default function App() {
   return (
     <>
       <Routes>
-        <Route />
+        <Route path='/' element={<Home />} />
       </Routes>
     </>
   )
