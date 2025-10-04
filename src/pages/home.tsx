d8068d0965da389aa5212738d0d57381c4e4373c
pietro <pietro.peerani@gmail.com>
2025-10-04 16:14:23 +0200

    update: home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,16 @@
-import Catalog from "../components/catalog";
-import FavoriteStationBar from "../components/favorite-station-bar";
-import Header from "../components/header";
-import Listened from "../components/listened";
+import { useState } from "react";
+import Catalog from "../lib/components/catalog";
+import FavoriteStationBar from "../lib/components/favorite-station-bar";
+import Header from "../lib/components/header";
+import Listened from "../lib/components/listened";
+import stations from "../lib/data/stations";
+import StationCard from "../lib/components/station-card";
+import AudioOnlyYouTubePlayer from "../lib/components/player";
 
 export default function Home() {
+  const [videoUrl, setVideoUrl] = useState<string>(stations[0].source);
+
+  console.log(videoUrl);
   return (
     <>
       <Header />
@@ -16,14 +23,20 @@ export default function Home() {
           <Catalog />
         </div>
       </div>
-      {/* <StationCard
+      <StationCard
         props={{
           name: "Jazz Vibes",
-          image: "https://plus.unsplash.com/premium_photo-1673177667569-e3321a8d8256?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y292ZXIlMjBwaG90b3xlbnwwfHwwfHx8MA%3D%3D",
-          followers: 204,
+          image:
+            "https://plus.unsplash.com/premium_photo-1673177667569-e3321a8d8256?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y292ZXIlMjBwaG90b3xlbnwwfHwwfHx8MA%3D%3D",
+          author: "John Doe",
+          source: "",
         }}
         square
-      /> */}
+        onClick={() =>
+          setVideoUrl("https://www.youtube.com/watch?v=jfKfPfyJRdk")
+        }
+      />
+      <AudioOnlyYouTubePlayer externalUrl={videoUrl} />
     </>
   );
 }
